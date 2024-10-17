24dbbc2e9f43f64dd9f630781ab4a9d4b28b243e
ligusx-build <37890938+ligusx-build@users.noreply.github.com>
2024-10-17 11:21:31 +0800

    Update x86-64-diy.sh

--- diy/x86-64-diy.sh
+++ diy/x86-64-diy.sh
@@ -6,7 +6,7 @@
 # 自定义操作1：添加额外的feeds
 echo -e "配置常用包..."
 echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> ./feeds.conf.default
-echo "src-git small https://github.com/kenzok8/small" >> ./feeds.conf.default
+echo "src-git smpackage https://github.com/kenzok8/small-package" >> ./feeds.conf.default
 #echo "src-git " >> ./feeds.conf.default
 
 # 自定义操作2：修改默认IP
