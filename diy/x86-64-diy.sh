eee1e0f29df08e78aa90dfff0d7a6f5fca34dcda
ligusx-build <37890938+ligusx-build@users.noreply.github.com>
2025-09-30 15:13:54 +0800

    Update x86-64-diy.sh

--- diy/x86-64-diy.sh
+++ diy/x86-64-diy.sh
@@ -5,8 +5,6 @@
 
 # 自定义操作1：添加额外的feeds
 echo -e "配置常用包..."
-echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> ./feeds.conf.default
-echo "src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;main" >> ./feeds.conf.default
 echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> ./feeds.conf.default
 echo "src-git small https://github.com/kenzok8/small" >> ./feeds.conf.default
 
@@ -16,7 +14,8 @@ sed -i 's/192.168.1.1/10.0.0.2/g' package/base-files/files/bin/config_generate
 
 # 自定义操作3：添加额外的包或配置
 echo "添加额外软件包..."
-
+# turboacc加速
+curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
 # 举例添加一个包
 #git clone https://github.com/yourusername/yourrepo.git package/yourrepo
 # 或者在feeds安装后使用
